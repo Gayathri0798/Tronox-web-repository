2efb5a3a1fdd77ad1a9105762b64455875e12961
Gayathri0798 <gayathri.subbiah@in.ey.com>
2025-03-11 11:36:52 +0530

    config change

--- Tronox-UI-Framework/wdio.conf.js
+++ Tronox-UI-Framework/wdio.conf.js
@@ -64,7 +64,7 @@ export const config = {
     capabilities: [{
         browserName: 'chrome',
         'goog:chromeOptions': {
-            args: []
+            args: ['--user-data-dir=/tmp/chrome-profile-unique']
         }
     }],
 
